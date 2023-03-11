1702c3aefd902d71640a8923d4a23cf649853f7c
Deda3d <alekspains8@gmail.com>
2023-03-11 14:35:55 +0200

    Добавьте файлы проекта.

---
+++ Лабораторна 1. Завдання 3/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int L = 5;
+        string[] A = { "ABCDE", "WXYZA", "MNOPQ"};
+
+        string result = A.Where(x => x.Length == L).OrderByDescending(x => x).FirstOrDefault();
+
+        Console.WriteLine(result);
+    }
+}
